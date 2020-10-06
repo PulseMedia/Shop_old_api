9491489a7b35fa9818d4acde83653381a6bf73f8
Pulse-Git <47848626+Pulse-Git@users.noreply.github.com>
2020-10-06 11:34:46 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,96 @@
+# BlueLunar NodeJs Webhook Http-Server Client
+
+Simple NodeJS Http-Server BlueLunar Integration
+
+### Requirements
+
+The Integration has the following requirements:
+
+* BlueLunar Seller Account
+* Server with NodeJs (12.18.4 LTS+) (Should be online 24/7)
+-- NodeJS Http-Server (BuiltIn)
+-- NPM [cli-color](https://www.npmjs.com/package/cli-color) (Optional, for Colored Console outputs)
+* Any Database or something else
+--to store the User Payments/Subscriptions eg(MongoDB (NPM mongoose))
+
+### Useage
+
+##### Initialization - Example
+Import the BlueLunar-NodeJs Module and Initialize the Client
+```js
+var BlueLunarNode = require('./nodeBlueLunar');
+var BlueLunar = new BlueLunarNode.client({
+  token: "<Your-Token>",
+  auth: "<Your-Auth>",
+  webhook: "/bl/webhook/" //Route to Webhook
+});
+if(BlueLunar.ready){
+    //Client Ready
+    //Start HttpServer or do other Actions
+}
+```
+##### Http-Server - Example
+
+
+```js
+http.createServer(function (request, response) {
+    if(BlueLunar.isWebhookRequest(request)){ //Check if request is an BlueLunar Webhook
+        BlueLunar.handleWebhook(request, response, (type, status, payload, user, actions) => { //Handle the Webhook
+            //type (string) == "PAYMENT" or "SUBSCRIPTION"
+            
+            //status (string) == "PAYMENT": type
+                // - "DONE" Payment is Done
+            //status (string) == "SUBSCRIPTION": type
+                // - "ACTIVE" Subscription Activated (After first Payment)
+                // - "CANCELLED" Subscription Cancelled by User
+                // - "SUSPENDED" Subscription Suspended by user (Maybe not Payed)
+                // - "EXPIRED" Subscription Expired (eg: Subscription only runs 3 Months)
+                
+            //payload (object)
+                //oid (string) - Unique Order ID
+                //article (string) - Article SKU
+                //quantity (number) - Buyed Quantity (on Subscription always 1)
+                
+            //user (object)
+                //<Dynamic Key/Value> User-Input Fields of the Product Page
+                
+            //actions (object)
+                //reject (function) execute if webhook was not successfully handelt
+                //resolve (function) execute if webhook was successfully handelt
+                
+            //EXAMPLE Payment Webhook Handling with DONE Status & ArticleID
+            if(type == "PAYMENT" && status == "DONE"){
+                if(payload.article == "123"){
+                    //Actions, eg: Save payment with user-data to database
+                    
+                    //Note its import to save the OrderID (payload.oid)
+                    //This is Important for Later Reference to the BlueLunar Payment/Subscription
+                    actions.resolve(); //Webhook Done
+                } else {
+                    //Invalid ArticleID
+                    actions.reject(); //Webhook Failed
+                }
+            } else {
+                //Invalid PaymentType/Status
+                actions.reject(); //Webhook Failed
+            }
+        }
+    } else {
+        //Do something else if it's not a webhook
+        response.end("Not A Webhook");
+    }
+}).listen(8080);
+
+//from above "Initialization"-Example, your WebHook url will be:
+//http://<YourIp/Domain>:8080/bl/webhook/
+```
+
+### Client Props & Methods
+
+The "parse"-Method returns the following Object with Keys/Values
+
+| Client.<Key> | Method/Returns |
+| ------ | ------ |
+| ready | Boolean - Check if Client is ready |
+| isWebhookRequest | Boolean - Check if Http-Server Request is an Webhook |
+| handleWebhook | Function - Handle the Webhook |
---
+++ nodeBlueLunar.js
@@ -0,0 +1,152 @@
+//Optional Package for colored console outputs:
+//cli-color (https://www.npmjs.com/package/cli-color)
+
+try {
+    var clc = require('cli-color');
+} catch (ex) {
+    var clc = undefined;
+}
+
+function blLog(message){
+  if(clc == undefined){
+    console.log("[BLUELUNAR] " + message);
+  } else {
+    console.log("["+ clc.xterm(45)("BLUELUNAR") +"] " + message);
+  }
+}
+
+var __$BlueLunar_Token = "";
+
+class BlueLunarClient{
+
+  constructor(settings){
+    if(settings.token){
+      this.Token = settings.token.trim();
+      __$BlueLunar_Token = this.Token;
+    } else {
+      blLog("Missing Token");
+      return this.notReady();
+    }
+    if(settings.auth){
+      this.Auth = settings.auth.trim();
+    } else {
+      blLog("Missing Auth");
+      return this.notReady();
+    }
+    if(settings.webhook){
+      if(!settings.webhook.startsWith("/")){
+        settings.webhook = "/" + settings.webhook;
+      }
+      if(settings.webhook.endsWith("/")){
+        this.Listen = settings.webhook.substring(0, settings.webhook.length-1);
+        this.ListenSlash = settings.webhook;
+      } else {
+        this.Listen = settings.webhook;
+        this.ListenSlash = settings.webhook + "/";
+      }
+    } else {
+      blLog("Missing Webhook path");
+      return this.notReady();
+    }
+
+    if(clc == undefined){
+      blLog("Client Ready");
+    } else {
+      blLog(clc.xterm(84)("Client Ready"));
+    }
+    this.ready = true;
+  }
+
+  log(message){
+    blLog(message);
+  }
+
+  notReady(){
+    this.ready = false;
+    if(clc == undefined){
+      blLog("Client Initialization Failed");
+    } else {
+      blLog(clc.xterm(197)("Client Initialization Failed"));
+    }
+  }
+
+  resolveLog(id){
+    if(clc == undefined){
+      blLog(id + "RESOLVED");
+    } else {
+      blLog(id + clc.xterm(84)("RESOLVED"));
+    }
+  }
+
+  rejectLog(id){
+    if(clc == undefined){
+      blLog(id + "REJECTED");
+    } else {
+      blLog(id + clc.xterm(198)("REJECTED"));
+    }
+  }
+
+  isWebhookRequest(request){
+    if(request.method == 'POST') {
+      if(request.url.trim() == this.Listen || request.url.trim() == this.ListenSlash){
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  handleWebhook(request, response, callback){
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    let data = "";
+    request.on("data", text => data += text);
+    request.once("end", () => {
+      let json = {};
+      try {
+        json = JSON.parse(data);
+      }
+      catch (err) {
+        blLog("Unable to parse WebhookJson");
+        response.end(JSON.stringify({ "error": "InvalidPostJson" }));
+        return;
+      }
+      if(json.auth && json.auth == this.Auth){
+        if(json.type && json.status && json.payload && json.user){
+
+          if(clc == undefined){
+            blLog(json.type + ": " + json.payload.oid);
+          } else {
+            blLog(clc.xterm(42)(json.type) + ": " + clc.xterm(75)(json.payload.oid));
+          }
+          callback(json.type, json.status, json.payload, json.user, {
+            resolve(){
+              response.end(JSON.stringify({ "token": __$BlueLunar_Token }));
+            },
+            reject(){
+              response.end(JSON.stringify({ "error": "Rejected" }));
+            }
+          })
+
+        } else {
+          json.auth = "<HIDDEN>";
+          blLog("Invalid Webhook Json\n" + JSON.stringify(json));
+          response.end(JSON.stringify({ "error": "InvalidJsonContent" }));
+        }
+      } else {
+        blLog("Invalid Webhook Auth");
+        response.end(JSON.stringify({ "error": "InvalidAuth" }));
+      }
+
+    });
+  }
+
+}
+
+module.exports = {
+
+  client: BlueLunarClient,
+
+};
